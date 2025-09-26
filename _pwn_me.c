8693a0ac6a9683d320fac26a7d92cd1d2d696e02
Arthur BIDET <arthur.bidet@gmail.com>
2025-09-26 18:25:11 +0900

    buff writing was too small

--- _pwn_me.c
+++ _pwn_me.c
@@ -3,7 +3,7 @@
 
 void vuln(){
     char buffer[32];
-    if (fgets(buffer, 0x32, stdin) != NULL) {
+    if (fgets(buffer, 0x100, stdin) != NULL) {
         printf("%s, you have a great name.\n", buffer);
     } else {
         printf("Error reading input.\n");
